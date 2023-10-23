3ef7713a78ac008cdafdaf81ee1ee63d3199b421
then00bprogrammer <lcs2021006@iiitl.ac.in>
2023-10-23 23:16:45 +0530

    bug fix

--- frontend/src/pages/projects.tsx
+++ frontend/src/pages/projects.tsx
@@ -85,7 +85,8 @@ const projects = () => {
           spacing={0}
         >
           <Heading
-            size={["4xl", "7xl"]}
+            fontSize={['4xl','7xl']}
+            fontWeight="extrabold"
             mb="2.5vh"
             color={useColorModeValue("gray.700", "yellow.100")}
           >
